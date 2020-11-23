8f70b82ddb47abef5cb548b6a5b628b5d9135cc5
unknown <mojtelefon00@gmail.com>
2020-11-23 19:52:56 +0100

    hooks seting

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,4 +1,4 @@
-import { withRouter, RouterProps } from 'next/router';
+import { withRouter } from 'next/router';
 
 export interface IProduct {
   id: string;
@@ -11,7 +11,7 @@ export interface IProduct {
 }
 interface IProductProps {
   product: IProduct;
-  router: RouterProps;
+  router;
 }
 const Product = (props: IProductProps) => {
   return (
